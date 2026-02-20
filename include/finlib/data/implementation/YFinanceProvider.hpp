0a1a3fd01321326b79754cca9eb9553f03fa84ed
JB BLET Linux <blet.jb@gmail.com>
2026-02-20 17:04:14 +0900

    Started working on implementing a View that allow the visualization of data

--- include/finlib/data/implementation/YFinanceProvider.hpp
+++ include/finlib/data/implementation/YFinanceProvider.hpp
@@ -1,20 +1,22 @@
+// "Copyright (c) 2026 JBBLET All Rights Reserved."
 #pragma once
+
+#include<utility>
+#include<string>
 #include "finlib/data/interfaces/ITimeSeriesLoader.hpp"
 
 class YFinanceProvider : public ITimeSeriesLoader {
-
-public:
+ public:
   explicit YFinanceProvider(
       std::string python_exec ="/home/jbblet/.venvs/finlib-python/bin/python",
-      std::string script_path ="src/scripts/YFinance_loader.py"
-      ): python_(std::move(python_exec)), script_path_(std::move(script_path)){}
+      std::string script_path ="/home/jbblet/Documents/Projects/FinLib/src/scripts/YFinance_loader.py")
+        : python_(std::move(python_exec)), script_path_(std::move(script_path)) {}
   TimeSeries load(
       const std::string& name,
       int64_t start_ts,
-      int64_t end_ts
-      ) override;
+      int64_t end_ts) override;
 
-private:
+ private:
   std::string python_;
   std::string script_path_;
 };
