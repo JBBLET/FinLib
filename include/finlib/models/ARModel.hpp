9e753648dbc2b2c30c6a8a30d6eb8457d4c31dc8
JB BLET Linux <blet.jb@gmail.com>
2026-03-19 23:33:26 +0900

    fix

--- include/finlib/models/ARModel.hpp
+++ include/finlib/models/ARModel.hpp
@@ -20,12 +20,26 @@ class ARModel : public BaseModel {
     std::string name() const override { return std::format("AR ({})", q_); };
     // std::string print() const override;
 
+    // Setters and Getters
+    bool requiresRegularSpacing() const override { return true; }
+    double regularityTolerance() const override { return regularityTolerance_; }
+    void setRegularityTolerance(double tolerance) {
+        regularityTolerance_ = tolerance;
+        if (!fullView_->checkRegularity(regularityTolerance_).isRegular) {
+            isFitted_ = false;
+        }
+    }
+    void setSolver(ARModel::Solver solver) {
+        if (solver != solver_) {
+            isFitted_ = false;
+            solver_ = solver;
+        }
+    }
+
+    // Interface implementation
     void fit() override;
     EvaluationResult evaluate(const TimeSeriesView& view) override;
     double predictOneStep(const Eigen::VectorXd& window) const override;
-
-    bool requiresRegularSpacing() const override { return true; }
-    double regularityTolerance() const override { return regularityTolerance_; }
 
     bool isStationary() const;
 
@@ -40,6 +54,9 @@ class ARModel : public BaseModel {
     double intercept_;
     double sigmaEpsilon_;
     Eigen::MatrixXd covarianceMatrix_;
+    Eigen::VectorXd standardErrors_;
+    Eigen::VectorXd tStatistics_;
+    Eigen::VectorXd pvalues_;
     // Methods
     void yuleWalkerSolver_();
     void leastSquareSolver_(const Eigen::MatrixXd& X, const Eigen::VectorXd& Y);
